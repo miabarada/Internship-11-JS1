0eccdb3581909bf71d06cfc9f3cbfe6f6bc1bbcf
miabarada <mia.barada2005@gmail.com>
2026-01-30 01:56:33 +0100

    add shift mode

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 const display = document.querySelector('.display');
 const buttons = document.querySelector('.buttons');
+const shiftBottun = document.querySelector('.shift-button');
 
 let displayValue = '';
 let firstOperand = null;
 let operator = null;
 let secondOperand = null;
+let inShiftMode = null;
 
 const buttonsDefinition = [
    { label: '7', type: 'number' },
    { label: '8', type: 'number' },
    { label: '9', type: 'number' },
-   { label: '+', shiftlabel: '!', type: 'operator', func: (a, b) => a + b, shiftFunc: factoriel},
+   { label: '+', unary: 'no', shiftLabel: '!', shiftUnary: 'yes', type: 'operator', func: (a, b) => a + b, shiftFunc: factoriel},
    { label: '4', type: 'number' },
    { label: '5', type: 'number' },
    { label: '6', type: 'number' },
-   { label: '-', shiftlabel: 'x³', type: 'operator', func: (a, b) => a - b, shiftFunc: (a) => a*a*a },
+   { label: '-', unary: 'no', shiftLabel: 'x³', shiftUnary: 'yes', type: 'operator', func: (a, b) => a - b, shiftFunc: (a) => a*a*a },
    { label: '1', type: 'number' },
    { label: '2', type: 'number' },
    { label: '3', type: 'number' },
-   { label: 'x', shiftlabel: 'log', type: 'operator', func: (a, b) => a * b, shiftFunc: (a) => Math.log10(a) },
+   { label: 'x', unary: 'no', shiftLabel: 'log', shiftUnary: 'yes', type: 'operator', func: (a, b) => a * b, shiftFunc: (a) => Math.log10(a) },
    { label: '=', type: 'equals' },
    { label: '0', type: 'number' },
-   { label: 'x²', shiftlabel:'√', type: 'operator', func: (a) => a * a, shiftFunc: (a) => Math.sqrt(a) },
-   { label: '/', shiftlabel:'³√', type: 'operator', func: (a, b) => b === 0 ? 'MATH ERROR' : a / b, shiftFunc: (a) => Math.cbrt(a) }
+   { label: 'x²', unary: 'yes', shiftLabel:'√', shiftUnary: 'yes', type: 'operator', func: (a) => a * a, shiftFunc: (a) => Math.sqrt(a) },
+   { label: '/', unary: 'no', shiftLabel:'³√', shiftUnary: 'yes', type: 'operator', func: (a, b) => b === 0 ? 'MATH ERROR' : a / b, shiftFunc: (a) => Math.cbrt(a) }
 ];
 
 function factoriel(a) {
    let result = 1;
    for(let i = 1; i <= a; i++)
       result *= i;
+   return result;
 }
 
 buttonsDefinition.forEach(definition => {
@@ -51,41 +54,40 @@ function handleClick(button) {
          return;
 
       firstOperand = Number(displayValue);
-      operator = button.label;
+      operator = button;
       displayValue = '';
       display.textContent = displayValue;
    }
 
    if (button.type === 'equals') {
-      if (displayValue === '' || operator === null)
+      if (operator === null)
          return;
 
-      secondOperand = Number(displayValue);
       let result;
 
-      if(operator === '+')
-         result = firstOperand + secondOperand;
-
-      if(operator === '-')
-         result = firstOperand - secondOperand;
-
-      if(operator === 'x')
-         result = firstOperand * secondOperand;
-
-      if(operator === '/') {
-         if (secondOperand === 0) {
-            display.textContent = 'MATH ERROR';
-            displayValue = '';
-            firstOperand = null;
-            operator = null;
-            return;
-         }
-         result = firstOperand / secondOperand;
+      if (inShiftMode && operator.shiftFunc) {
+         result = operator.shiftFunc(firstOperand);
+      } else {
+         if (displayValue === '') return;
+         secondOperand = Number(displayValue);
+         result = operator.func(firstOperand, secondOperand);
       }
 
       display.textContent = result;
       displayValue = result.toString();
-      return;
+      operator = null;
    }
+}
+
+shiftBottun.addEventListener('click', () => {
+   inShiftMode = !inShiftMode;
+   updateButtons();
+});
+
+function updateButtons() {
+   document.querySelectorAll('.buttons button').forEach((btn, index) => {
+      const data = buttonsDefinition[index];
+      btn.textContent = inShiftMode && data.shiftLabel ? data.shiftLabel : data.label;
+   })
 }
 
